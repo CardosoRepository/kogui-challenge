1b8a1109295ea4ef8d99f6bbbbb37d069b6c2eff
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-06-19 15:38:04 -0300

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CharactersComponent } from './characters/characters.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: 'characters', component: CharactersComponent },
+	{ path: '', redirectTo: 'characters', pathMatch: 'full'},
+	{ path: '**', component: NotFoundComponent},
+];
