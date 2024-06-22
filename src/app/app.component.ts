d53694eda9a827706a14e52ee3200cc853c33b02
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-06-22 19:43:10 -0300

    bug: improvements on components (styles and search bar)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@feature/header/header.component';
+import { SearchBarComponent } from '@shared/components/search-bar/search-bar.component';
+import { filter } from 'rxjs';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [CommonModule, RouterOutlet, HeaderComponent],
+	imports: [CommonModule, RouterOutlet, HeaderComponent, SearchBarComponent],
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+	showSearchBar: boolean = false;
 	title = 'rick-and-morty-kogui-challenge';
+
+	constructor(private _router: Router) {}
+
+	ngOnInit(): void {
+		this._router.events
+			.pipe(filter((event) => event instanceof NavigationEnd))
+			.subscribe((event: any) => {
+				this.showSearchBar = this._isListRoute(event.urlAfterRedirects);
+			});
+	}
+
+	private _isListRoute(url: string): boolean {
+		const baseURL = url.split('?')[0];
+		return ['/characters', '/locations', '/episodes'].some((route) => baseURL === route);
+	}
+
 }
--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,31 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router, RouterModule } from '@angular/router';
-import { SearchBarComponent } from '@shared/components/search-bar/search-bar.component';
-import { filter } from 'rxjs';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
 	selector: 'app-header',
 	standalone: true,
-	imports: [CommonModule, RouterModule, SearchBarComponent],
+	imports: [CommonModule, RouterModule],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss',
 })
-export class HeaderComponent implements OnInit {
-	showSearchBar: boolean = false;
-
-	constructor(private _router: Router) {}
-
-	ngOnInit(): void {
-		this._router.events
-			.pipe(filter((event) => event instanceof NavigationEnd))
-			.subscribe((event: any) => {
-				this.showSearchBar = this._isListRoute(event.urlAfterRedirects);
-			});
-	}
-
-	private _isListRoute(url: string): boolean {
-		const baseURL = url.split('?')[0];
-		return ['/characters', '/locations', '/episodes'].some((route) => baseURL === route);
-	}
-}
+export class HeaderComponent {}
