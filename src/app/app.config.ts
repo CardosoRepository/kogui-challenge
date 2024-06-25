73b4ca59bc41d2a33a7855b3f9b4309c9ada1b90
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-06-25 12:17:05 -0300

    bug: Hash Location URL

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
+import { PreloadAllModules, provideRouter, withHashLocation, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(routes, withPreloading(PreloadAllModules)), provideHttpClient()],
+	providers: [provideRouter(routes, withPreloading(PreloadAllModules), withHashLocation()), provideHttpClient()],
 };
